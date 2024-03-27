b714821d2fc14396da131a84575612e7045481b2
Alex Moore <47441506+waba1111@users.noreply.github.com>
2024-03-27 16:30:16 -0700

    Update gbfr_damage_calculator_seed.sql
    added table values for over_masteries table

--- gbfr_damage_calculator_seed.sql
+++ gbfr_damage_calculator_seed.sql
@@ -1,4 +1,16 @@
 /* queries to insert all data into database. */
 -- TODO:
-INSERT INTO ()
-VALUES ()
+INSERT INTO over_masteries(attribute, values)
+VALUES (
+('Attack Power Up',	[200, 300,400,500,600,700,800,1000]),
+('HP Up',	[400,500,600,800,1000,1200,1600,2000]),
+('Critical Hit Rate Up',	[0.02,0.04,0.06,0.08,0.1,0.12,0.16,0.2]),
+('Stun Power Up',	[2,4,6,8,10,12,16,20]),
+('Skill Damage Up',	[0.02,0.04,0.06,0.08,0.1,0.12,0.16,0.2]),
+('Skybound Art Damage Up',	[0.02,0.04,0.06,0.08,0.1,0.12,0.16,0.2]),
+('Chain Burst Damage Up',	[0.02,0.04,0.06,0.08,0.1,0.12,0.16,0.2]),
+('Normal Attack Damage Cap Up',	[0.02,0.04,0.06,0.08,0.1,0.12,0.16,0.2]),
+('Skill Damage Cap Up',	[0.02,0.04,0.06,0.08,0.1,0.12,0.16,0.2]),
+('Skybound Art Damage Cap Up',	[0.02,0.04,0.06,0.08,0.1,0.12,0.16,0.2]),
+('Healing Cap Up',	[0.02,0.04,0.06,0.08,0.1,0.12,0.16,0.2])
+)
